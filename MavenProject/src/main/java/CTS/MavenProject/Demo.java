1ddfdfc5cf9bebb76060dbab5bd3322aeaca3301
srini <srini@srini-PC>
2018-02-01 21:09:48 +0530

    1st Commit

---
+++ MavenProject/src/main/java/CTS/MavenProject/Demo.java
@@ -0,0 +1,12 @@
+package CTS.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void MyTest() throws Exception
+	{
+		System.out.println("Maven Project i Installed");
+	}
+}
