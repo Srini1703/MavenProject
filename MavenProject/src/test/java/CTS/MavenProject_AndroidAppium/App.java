f6cf879970f7fce7c84f15cca987574d9e6c35b6
srini <srini@srini-PC>
2018-06-02 14:14:15 +0530

    modified

---
+++ MavenProject/src/test/java/CTS/MavenProject_AndroidAppium/App.java
@@ -0,0 +1,16 @@
+package CTS.MavenProject_AndroidAppium;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public void myTest()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
