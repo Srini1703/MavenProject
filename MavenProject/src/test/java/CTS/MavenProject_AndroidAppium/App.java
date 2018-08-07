7e1d0ba1f2aac4fd90811ee934db35ccb9588214
srini <srini@srini-PC>
2018-08-07 22:11:45 +0530

    Speech to text and text to speech inside iFrame committed

--- MavenProject/src/test/java/CTS/MavenProject_AndroidAppium/App.java
+++ MavenProject/src/test/java/CTS/MavenProject_AndroidAppium/App.java
@@ -1,7 +1,13 @@
 package CTS.MavenProject_AndroidAppium;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 /**
@@ -12,11 +18,34 @@ public class App
 {
 	WebDriver driver;
 	@Test
-    public void myTest()
+    public void lauchBrowser(String browserName)
     {
-        System.out.println( "Hello World!" );
-        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://www.google.co.in/");
+		/*browserName = browserName.toLowerCase();
+		switch(browserName) {
+		
+		case"chrome":
+	        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case"firefox":
+			System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		case"ie":
+			System.setProperty("webdriver.ie.driver", "./Drivers/internetexplorerdriver.exe");
+			driver = new InternetExplorerDriver();
+			break;	
+		}*/
+		
+		//selectByVisibleText
+		
+		WebElement element = driver.findElement(By.id("Country"));
+		Select oSelect = new Select(element);
+		oSelect.selectByVisibleText("2010");
+		//By index
+		oSelect.selectByIndex(4);
+		//By Value
+		oSelect.selectByValue("India");
+		Alert a = driver.switchTo().alert();
     }
 }
