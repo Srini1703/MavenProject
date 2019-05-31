e55d54081efc9cd34f62e633f1b11b64d64540cf
srini <srini@srini-PC>
2019-05-31 19:38:42 +0530

    Modified Data Provider

--- MavenProject/src/main/java/CTS/Utlities/DataProvider.java
+++ MavenProject/src/main/java/CTS/Utlities/DataProvider.java
@@ -1,11 +1,19 @@
 package CTS.Utlities;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 
 public class DataProvider {
+	WebDriver driver;
 	
 	@SuppressWarnings("rawtypes")
 	@org.testng.annotations.DataProvider(name="myTC")
@@ -20,4 +28,10 @@ public class DataProvider {
 		}
 		return dataArr.iterator();
 	}
+	
+	FluentWait<WebDriver> fWait = new FluentWait<WebDriver>(driver)
+			.withTimeout(Duration.ofSeconds(30))
+			.pollingEvery(Duration.ofMillis(500))
+			.ignoring(TimeoutException.class)
+			.ignoring(NoSuchElementException.class);
 }
