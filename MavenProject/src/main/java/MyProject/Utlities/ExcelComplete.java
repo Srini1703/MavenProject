527075e17b506dfa688d307e6e0d40540e898d2a
srini <srini@srini-PC>
2020-02-02 18:22:59 +0530

    Modified Code

---
+++ MavenProject/src/main/java/MyProject/Utlities/ExcelComplete.java
@@ -0,0 +1,47 @@
+package MyProject.Utlities;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelComplete {
+	
+	@SuppressWarnings({ "resource", "deprecation" })
+	public ArrayList<HashMap<String,String>> readExcel() throws Exception{
+		String heading="",key="";
+		ArrayList<HashMap<String,String>> arrMap = new ArrayList<HashMap<String,String>>();
+		FileInputStream fis = new FileInputStream("");
+		//XSSFWorkbook wb1 = new XSSFWorkbook(fis);
+		HSSFWorkbook wb = new HSSFWorkbook(fis);
+		HSSFSheet sh = wb.getSheet("");
+		int rowCount = sh.getLastRowNum() - sh.getFirstRowNum();
+		for(int i=0;i<=rowCount;i++) {
+			HashMap<String,String> hm = new HashMap<String,String>();
+			Row row = sh.getRow(i);
+			int colCount = row.getLastCellNum() - row.getFirstCellNum();
+			for(int j=0;j<=colCount;j++) {
+				heading = sh.getRow(0).getCell(j).getStringCellValue();
+				Cell cel = row.getCell(j);
+				switch(cel.getCellType()) {
+					case Cell.CELL_TYPE_STRING :
+						key = cel.getStringCellValue();
+						break;
+				}
+				hm.put(heading, key);
+			}
+			arrMap.add(hm);
+		}
+		
+		return arrMap;
+	}
+	
+	
+	
+}
